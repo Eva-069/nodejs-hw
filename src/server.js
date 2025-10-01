4a48a66042c8aaea88f68e62cd87c2c35d44b7b7
Eva <evatereshenko12321@gmail.com>
2025-10-01 17:47:50 +0300

    end

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ app.get('/notes', (req, res) => {
   res.status(200).json({ message: "Retrieved all notes" });
 });
 
-app.get('/notes:noteId', (req, res) => {
+app.get('/notes/:noteId', (req, res) => {
   const idParam = req.params.noteId;
   res.status(200).json({ message: `Retrieved note with ID:${idParam}` });
 });
@@ -59,7 +59,6 @@ app.use((err, req, res, next) => {
   console.error('Error:', err.message);
   res.status(500).json({
    message: 'Internal Server Error',
-    error: err.message,
   });
 });
 
